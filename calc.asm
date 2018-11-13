0f9b531ff8a9fbf75dedacc835d882a35607823e
jdk <zhe.dan28@gmail.com>
2018-11-13 21:37:08 +0500

    Solved calc task for Linux x64

--- calc.asm
+++ calc.asm
@@ -2,17 +2,16 @@
     operand_1 = 10
     operand_2 = 2
     operation = '/'
-    message: 
-        .asciz "Result: %d\n"
+    error_message:
+        .asciz "Not known operation\n"
+    l_error_message = . - error_message
+    line_feed: .byte 0x0A
+    minus: .byte 0x2D
     
-.extern printf
 .text
-    .global main
+    .global _start
     
-main:
-    movq %rsp, %rbp
-    subq $32, %rsp
-    
+_start:
     movq $operation, %rcx
     cmpq $0x2B, %rcx
     je _sum
@@ -22,31 +21,85 @@ main:
     je _sub
     cmpq $0x2F, %rcx
     je _div
- 
- _print:
-    movq $message, %rcx
-    movq %rax, %rdx
-    call printf
-    
-    movq %rbp, %rsp
+    jmp _error
+
+_error:
+    mov $error_message, %rsi
+    mov $l_error_message, %rdx
+    call _console_write
+    jmp _exit_error
+
+_console_write:
+    movq $1, %rax
+    movq $1, %rdi
+    syscall
     ret
+ 
+_pre_process:
+    cmpq $0, %rax
+    jge _process
+    neg %eax
+    push %rax
+    movq $minus, %rsi
+    movq $1, %rdx
+    call _console_write
+    pop %rax
+    jmp _process
+
+_process:
+    xorq %r10, %r10
+_process_step:
+    movq $0, %rdx
+    movq $10, %rbx
+    divq %rbx
+    addq $48, %rdx
+    push %rdx
+    incq %r10
+    cmpq $0, %rax
+    jz _print       
+    jmp _process_step
+
+ _print:
+    cmpq $0, %r10
+    jz _exit_success
+    decq %r10
+    mov %rsp, %rsi
+    movq $1, %rdx
+    call _console_write
+    add $8, %rsp
+    jmp _print
+
+_exit_error:
+    movq $1, %rdi
+    jmp _exit
+
+_exit_success:
+    movq $line_feed, %rsi
+    movq $1, %rdx
+    call _console_write
+    movq $0, %rdi
+    jmp _exit
+
+_exit:
+    movq $60, %rax
+    syscall
 
 _sum:
     movq $operand_1, %rax
     addq $operand_2, %rax
-    jmp _print
+    jmp _pre_process
 
 _sub:
     movq $operand_1, %rax
     subq $operand_2, %rax
-    jmp _print
+    jmp _pre_process
     
 _mul:
     xorq %rax, %rax
     movq $operand_2, %rcx
     mov $operand_1, %al
     mul %rcx
-    jmp _print
+    jmp _pre_process
 
 _div:
     xorq %rax, %rax
@@ -54,4 +107,4 @@ _div:
     movq $0, %rdx
     movq $operand_1, %rax
     div %rcx
-    jmp _print
+    jmp _pre_process
