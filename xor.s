bec8237a8e033080ab29387849b7a5a7c11309e5
jdk <zhe.dan28@gmail.com>
2018-11-14 00:56:29 +0500

    Solved xor task

--- xor.s
+++ xor.s
@@ -1,20 +1,24 @@
 .data
-.set N, 100
-.set OPEN, 2
-.set WRITE, 1
-.set READ, 0
-.set EXIT, 60
-.set O_RDONLY, 0
-msg:
-.ascii "Error!\n"
-len = . - msg
-filename: .asciz "1.txt"
-buffer: .skip N
+	.set N, 100
+	.set CLOSE, 3
+	.set OPEN, 2
+	.set WRITE, 1
+	.set READ, 0
+	.set EXIT, 60
+	.set O_RDONLY, 0
+	.set O_WRONLY, 1
+	msg:
+		.ascii "Error!\n"
+	len = . - msg
+	filename: .asciz "test.txt"
+	buffer: .skip N
+
 .text
-.globl _start
+	.globl _start
+
 _start:
-	mov $OPEN, %rax
-	lea filename, %rdi
+	movq $OPEN, %rax
+	mov 16(%rsp), %rdi
 	mov $O_RDONLY, %rsi
 	syscall
 	cmp $0, %rax
@@ -26,11 +30,35 @@ _start:
 	mov $N, %rdx
 	syscall	
 
+	mov 32(%rsp), %r10
+	mov (%r10), %r10
+_xor:
+	mov (%rsi), %cl
+	cmp $0, %cl
+	je _write_file
+	xor %r10, (%rsi)
+	inc %rsi
+	jmp _xor
+
+
+_write_file:
+	movq $OPEN, %rax
+	mov 24(%rsp), %rdi
+	mov $O_WRONLY, %rsi
+	syscall
+	cmp $0, %rax
+	jl _error
+
+	mov %rax, %rdi
 	mov $WRITE, %rax
-	mov $1, %rdi
 	mov $buffer, %rsi
 	mov $N, %rdx
 	syscall	
+
+	mov %rax, %rdi
+	mov $CLOSE, %rax
+	syscall
+
 	call _exit
 
 _error:
@@ -44,3 +72,5 @@ _exit:
 	mov $EXIT, %rax
 	mov $0, %rdi
 	syscall	
+
+
